b261a516cbbf715a5cb12c9d1630f03d08f51442
Shane Sutro <shanesutro@gmail.com>
2021-04-02 17:58:02 -0600

    Minor change 5

--- __init__.py
+++ __init__.py
@@ -2,8 +2,7 @@ from vestaboard import Formatter
 
 def main(*args, **kwargs):
   fm = Formatter()
-  return {'characters': fm.convert('The quick brown fox jumps over the lazy dog!')}
-  
+  return {'characters': fm.convert('The quick brown fox jumps over the lazy dog!!')}
 
 if __name__ == '__main__':
   print(main())
