6fa500906ff37e22a2f89e18ed0e112a23b8d6ab
Shane Sutro <shanesutro@gmail.com>
2021-04-02 10:54:22 -0600

    Initial test file

---
+++ __init__.py
@@ -0,0 +1,8 @@
+from vestaboard import Formatter
+
+def main(*args, **kwargs):
+  fm = Formatter()
+  return {'characters': fm.convert('The quick brown fox jumps over the lazy dog!')}
+
+if __name__ == '__main__':
+  print(main())
