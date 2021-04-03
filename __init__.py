623d365218f4d0267c5d97a3c86f83a9ca8d4a6d
Shane Sutro <shanesutro@gmail.com>
2021-04-02 18:19:39 -0600

    Change 12

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ from vestaboard import Formatter
 
 def main(*args, **kwargs):
   fm = Formatter()
-  return {'characters': fm.convert('ABC')}
+  return {'characters': fm.convert('ABCDEF')}
 
 if __name__ == '__main__':
   print(main())
