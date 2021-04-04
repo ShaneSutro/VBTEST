9c4a9a610653a0e17eebc6fce047ebd779134378
Shane Sutro <shanesutro@gmail.com>
2021-04-03 21:40:29 -0600

    Minor change 6

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from vestaboard import Formatter
 
 def main(*args, **kwargs):
-  print('Changed second args: ', args)
+  print('Here are the args: ', args)
   fm = Formatter()
   return {'characters': fm.convert('ABCDEF')}
 
